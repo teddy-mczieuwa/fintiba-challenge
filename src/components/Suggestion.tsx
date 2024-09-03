9eec57e9295754deadc6effefe264dd355e0dd7a
Edward <teddymczieuwa@gmail.com>
2024-09-03 09:55:30 +0200

    minor css update

--- src/components/Suggestion.tsx
+++ src/components/Suggestion.tsx
@@ -1,7 +1,19 @@
 import styled from "styled-components";
+import { MediaQuery } from "../devices";
 
 const SuggestionContainer = styled.div`
-  width: 20%;
+  @media ${MediaQuery.laptop} {
+    width: 20%;
+    margin-top: 0.5rem;
+  }
+
+  @media ${MediaQuery.tablet} {
+    width: 35%;
+  }
+
+  @media ${MediaQuery.mobile} {
+    width: 80%;
+  }
   margin: 0 auto;
   background-color: white;
   border-radius: 5px;
